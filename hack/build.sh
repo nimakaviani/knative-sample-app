c94a62b1803eeef11887754a410da4cff026a513
Nima Kaviani <nkavian@us.ibm.com>
2019-03-13 14:54:21 -0700

    fix build script
    Give an error when version is not supplied to the build script.

--- hack/build.sh
+++ hack/build.sh
@@ -2,8 +2,17 @@
 
 set -e
 
+if [ -z $VERSION ]; then
+    if [[ -z ${VERSION} ]] && [[ ! -z ${1} ]] ; then
+        VERSION=$1
+    else
+        echo "missing version: should be v1 or v2"
+        exit 1
+    fi
+fi
+
 go fmt github.com/nimakaviani/knative-sample-app/...
 
 # export GOOS=linux GOARCH=amd64
 
-go build -o out/server github.com/nimakaviani/knative-sample-app/app/${1}
+go build -o out/server github.com/nimakaviani/knative-sample-app/app/${VERSION}
